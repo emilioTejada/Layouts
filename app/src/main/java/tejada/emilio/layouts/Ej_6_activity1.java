4905a919566f6d3ce9a891a997a6276af1669bf5
Emilio Tejada <ejtb76@gmail.com>
2016-10-22 00:15:39 +0200

    hasta ej 10

---
+++ app/src/main/java/tejada/emilio/layouts/Ej_6_activity1.java
@@ -0,0 +1,13 @@
+package tejada.emilio.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ej_6_activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ej_6_activity1);
+    }
+}
--- app/src/main/java/tejada/emilio/layouts/ScrollingActivity.java
+++ app/src/main/java/tejada/emilio/layouts/ScrollingActivity.java
@@ -1,28 +1,32 @@
 package tejada.emilio.layouts;
 
-
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-
-public class Lanzador extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity {
     int NUM_EJERCICIOS = 20;
     int EJERCICIO_INICIAL=1;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lanzador);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.activity_lanzador);
+        setContentView(R.layout.activity_scrolling);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+
+      //  LinearLayout ll = (LinearLayout) findViewById(R.id.activity_lanzador);
 
         for (int i = EJERCICIO_INICIAL; i < EJERCICIO_INICIAL + NUM_EJERCICIOS; i++) {
             Button boton = new Button(this);
@@ -30,16 +34,22 @@ public class Lanzador extends AppCompatActivity {
             boton.setTag(i);
             // boton.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
 
-            LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            //LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
             //lparams.addRule(LinearLayout.ALIGN_PARENT_END, R.id.activity_lanzador);
-            boton.setLayoutParams(lparams);
+           // boton.setLayoutParams(lparams);
 
             boton.setOnClickListener(getOnClickDoSomething(boton));
 
-            ll.addView(boton);
+            //ll.addView(boton);
 
         }
+
+
+
+
     }
+
+
 
     View.OnClickListener getOnClickDoSomething(final Button button)  {
         return new View.OnClickListener() {
@@ -59,12 +69,5 @@ public class Lanzador extends AppCompatActivity {
             }
         };
     }
-
-
-
-
-//       Button bt = new Button(this);
-//        bt.setText("A Button");
-//
 
 }
